b8b6718ec54f9eb343b7adc243c17cc2dce43e64
GOAF-Pilot <GOAF-Pilot@users.noreply.github.com>
2017-09-28 19:15:39 -0700

    add bubblesorting functionality, and use future_print

--- pythonProgramming/bubbleSort.py
+++ pythonProgramming/bubbleSort.py
@@ -1,8 +1,26 @@
+from __future__ import print_function
+#because python3's print function is far superior
+
 print("enter some numbers to sort, type quit when you are done")
-numbers=[0]
-number=''
+numbers=[]
+number=0
 while number != 'quit':
     number=raw_input('input: ')
     if number!='quit':
         numbers.append(number)
-print('here are the unsorted numbers: ', numbers)
+        #number=ord(number)-97
+print("here are the unsorted numbers: ", numbers)
+
+flag=True
+for i in range (0, len(numbers)):
+    if flag==False:
+        break
+    flag=False
+    for j in range (0, len(numbers)-1):
+        if numbers[j+1]<numbers[j]:
+            transfer=numbers[j+1]
+            numbers[j+1]=numbers[j]
+            numbers[j]=transfer
+            flag=True
+
+print("here are the sorted numbers: ", numbers)
