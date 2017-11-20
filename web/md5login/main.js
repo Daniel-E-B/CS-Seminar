5a8d90a469cedc739faa0e9691b325e35e5cf513
GOAF-Pilot <GOAF-Pilot@users.noreply.github.com>
2017-11-20 11:06:34 -0800

    make some styling changes

--- web/md5login/main.js
+++ web/md5login/main.js
@@ -11,11 +11,16 @@ var users=[];
 users.push(new User("Admin","827ccb0eea8a706c4c34a16891f84e7b","Yay, you logged in!"))
 //the password is "12345"
 
+//make it so that other people can add users. Maybe a different html page?
+//have a file to store the hashes of users
+
 function formChanged(){
     var username=document.getElementById("usernameInput").value;
     var password=md5(document.getElementById("passwordInput").value);
     var user = getUser(username, password);
-    if (!user) return;
+    if (!user)
+        return;
+    
     document.getElementById("userContent").innerHTML=user.userData;
 }
 
@@ -27,4 +32,5 @@ function getUser(username,pass){
         
         return user;
     }
+    document.getElementById("userContent").innerHTML="Incorrect username or password";
 }
