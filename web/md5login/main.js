7435361a42a8a2e0756da6c063b3dafd47b2c264
GOAF-Pilot <GOAF-Pilot@users.noreply.github.com>
2017-11-20 10:14:26 -0800

    fixed the code. Thanks rohan

--- web/md5login/main.js
+++ web/md5login/main.js
@@ -1,42 +1,30 @@
-function User () {
-    this.username = "";
-    this.hashedPass="";
-    this.userData="";
-    this.getData = function() {
-        return this.userData;
-    };
-    this.getUsername=function(){
-        return this.username;
-    }
-    this.getHashedPass=function(){
-        return this.hashedPass;
-    }
-    this.init=function(name,pass,data){
+class User {
+    constructor(name,pass,data){
         this.username=name;
         this.hashedPass=pass;
         this.userData=data;
     }
 }
 
-var users=[]
-var stuffToDisplay="";
+var users=[];
 
-users.push(User("Admin","827ccb0eea8a706c4c34a16891f84e7b","Yay, you logged in!"))
+users.push(new User("Admin","827ccb0eea8a706c4c34a16891f84e7b","Yay, you logged in!"))
 //the password is "12345"
 
 function formChanged(){
     var username=document.getElementById("usernameInput").value;
     var password=md5(document.getElementById("passwordInput").value);
-    getUser(username,password);
-    document.getElementById("userContent").innerHTML=stuffToDisplay;
+    var user = getUser(username, password);
+    if (!user) return;
+    document.getElementById("userContent").innerHTML=user.userData;
 }
 
 function getUser(username,pass){
-    for(user in users){
-        if(username==user.getUsername()){
-            if(pass==user.getHashedPass()){
-                stuffToDisplay=user.getData();
-            }
-        }
+    for(i=0; i<users.length; i++){
+        var user = users[i];
+        if (username !== user.username || pass !== user.hashedPass)
+            continue;
+        
+        return user;
     }
 }
