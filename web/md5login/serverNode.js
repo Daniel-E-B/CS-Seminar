e9b2b11cfad281984592eba288552c14bc1f7aa7
GOAF-Pilot <GOAF-Pilot@users.noreply.github.com>
2017-11-20 19:33:40 -0800

    its still not working...how to node'
    qe
    
    q
    quit()
    
    '
    ?

--- web/md5login/serverNode.js
+++ web/md5login/serverNode.js
@@ -2,14 +2,15 @@ var fs=require('fs');
 
 var express=require('express');
 var http=require('http');
+var connect = require('connect');
 
-var app = express();
-app.set('port', process.env.PORT || 3000);
+var app = http.createServer().use(connect.static(__dirname + './'));
+app.listen(3000);
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 server.listen(app.get('port'));
 
   // wait for the event raised by the client
-  socket.on('userlist', function (data) {  
+  /*server.on('userlist', function (data) {  
     console.log(data);
-  });
+  });*/
