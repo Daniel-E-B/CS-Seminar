9f365ee8182075043fe0dab2d0e7e8a900e36ec1
GOAF-Pilot <GOAF-Pilot@users.noreply.github.com>
2017-12-01 14:22:00 -0800

    add oo items

--- web/final project/main/main.js
+++ web/final project/main/main.js
@@ -1,5 +1,14 @@
 var cookies=0;
 
+function item(){
+    this.cost;
+    this.CPSboost;
+}
+
+function friend (){}
+friend.prototype=Object.create(item);
+//friend "extends" item
+
 while(true){
-    
+
 }
