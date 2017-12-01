31aeddee30b927f0a0736c6a188e681843cc5471
GOAF-Pilot <GOAF-Pilot@users.noreply.github.com>
2017-12-01 15:28:23 -0800

    stuipd js

--- web/final project/main/main.js
+++ web/final project/main/main.js
@@ -1,14 +1,27 @@
 var cookies=0;
+var frames=0;
+var CPF;
 
 function item(){
-    this.cost;
-    this.CPSboost;
+    this.cost=0;
+    this.CPFboost=0;
 }
 
 function friend (){}
 friend.prototype=Object.create(item);
+friend.cost=15;//make it increment?
+friend.CPFboost=0.02;
 //friend "extends" item
 
-while(true){
+/*var items=[];
+items.push(new item());*/
 
+function update(){
+    //cookies+=CPF;
+    document.getElementById('numCookies').innerHTML=cookies;
+    //it cant find numCookies, probably b/c it hasnt been created
+}
+
+while(true){
+    update();
 }
--- web/final project/main/script.js
+++
@@ -1,7 +0,0 @@
-var cookies=0;
-
-function click(){
-    console.log(1);
-    cookies++;
-    document.getElementById("numCookies").innerHTML=cookies;
-}
