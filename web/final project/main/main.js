0925c72ff690d5137ef4dc7eecb3d4d9ff1f6af5
GOAF-Pilot <GOAF-Pilot@users.noreply.github.com>
2017-12-03 18:52:40 -0800

    basic idea is working

--- web/final project/main/main.js
+++ web/final project/main/main.js
@@ -1,27 +1,37 @@
-var cookies=0;
+var cookies=15;
 var frames=0;
-var CPF;
+var CPS=0;
 
-function item(){
-    this.cost=0;
-    this.CPFboost=0;
+class Item{
+    constructor(CPSBoost, cost){
+        this.CPSBoost=CPSBoost;
+        this.cost=cost;
+        if(cookies>=this.cost){
+            cookies-=this.cost;
+            CPS+=this.CPSBoost;
+        }
+    }
 }
 
-function friend (){}
-friend.prototype=Object.create(item);
-friend.cost=15;//make it increment?
-friend.CPFboost=0.02;
-//friend "extends" item
+class Helper extends Item{
+    constructor(){
+        super(1,15);
+    }
+}
 
-/*var items=[];
-items.push(new item());*/
+var updater;
 
 function update(){
-    //cookies+=CPF;
-    document.getElementById('numCookies').innerHTML=cookies;
+    cookies+=CPS;
+    document.getElementById('CPS').innerHTML=CPS;
+    document.getElementById('cookieCounter').innerHTML=cookies;
     //it cant find numCookies, probably b/c it hasnt been created
 }
 
-while(true){
-    update();
+var items=[];
+
+window.onload = function(){
+    items.push(new Helper());
+    updater = setInterval(update, 1000);
 }
+
